cd827003c86796461b1cc78ab82322a4e0237812
Eric K <54124297+Nyveon@users.noreply.github.com>
2021-11-12 02:32:13 -0300

    Setting persistence and checks

--- js/integrantes-viewer.js
+++ js/integrantes-viewer.js
@@ -3,58 +3,66 @@
  */
 
 //todo: don't render your own courses
-//todo: fix tooltip bounding box
 //todo: fix where number superscript is placed
-
 //todo: check settings
 
 /**
  * Next to every hyperlink-username, adds the superscript number of shared courses.
  * The superscript number has a tooltip which shows the specific courses shared.
  */
-chrome.storage.local.get("users", function (data) {
-    const users = data["users"] ?? {};
-    const userLinks = document.querySelectorAll('a[href *= "usuario"]');
 
-    for (const user of userLinks) {
-        const splitUser = user["href"].split("/");
+chrome.storage.local.get("settings", function (data) {
+    const settings = data["settings"] ?? {};
 
-        if (splitUser.length === 6) {
-            const thisID = splitUser[4];
-            if (users[thisID] !== undefined) {
+    // Break if currently not set to display this
+    if (!settings["cc-show-counter"]) {
+        return
+    }
 
-                const tooltip = document.createElement("div");
-                tooltip.classList.add("tutooltiptext");
+    chrome.storage.local.get("users", function (data) {
+        const users = data["users"] ?? {};
+        const userLinks = document.querySelectorAll('a[href *= "usuario"]');
 
-                const thisUser = users[thisID];
+        for (const user of userLinks) {
+            const splitUser = user["href"].split("/");
 
-                for (const [key, value] of Object.entries(thisUser)) {
-                    if (key !== "name") {
-                        const thisCourse = document.createElement("a");
-                        //todo: maybe switch to icon instead of text?
-                        //https://www.u-cursos.cl/d/images/cargos/profesor_auxiliar.svg lower case
+            if (splitUser.length === 6) {
+                const thisID = splitUser[4];
+                if (users[thisID] !== undefined && thisID !== settings["my-user"]) {
 
-                        thisCourse.classList.add("tucompartido");
-                        thisCourse.href = value["URL"];
-                        thisCourse.textContent = value["code"] + "-" + value["section"] + " ";
-                        thisCourse.textContent += "(" + value["year"] + "-" + value["semester"] + ")";
-                        thisCourse.textContent += "  |  " + value["theirType"];
-                        //listItem.appendChild(thisCourse);
-                        tooltip.appendChild(thisCourse);
+                    const tooltip = document.createElement("div");
+                    tooltip.classList.add("tutooltiptext");
+
+                    const thisUser = users[thisID];
+
+                    for (const [key, value] of Object.entries(thisUser)) {
+                        if (key !== "name") {
+                            const thisCourse = document.createElement("a");
+                            //todo: maybe switch to icon instead of text?
+                            //https://www.u-cursos.cl/d/images/cargos/profesor_auxiliar.svg lower case
+
+                            thisCourse.classList.add("tucompartido");
+                            thisCourse.href = value["URL"];
+                            thisCourse.textContent = value["code"] + "-" + value["section"] + " ";
+                            thisCourse.textContent += "(" + value["year"] + "-" + value["semester"] + ")";
+                            thisCourse.textContent += "  |  " + value["theirType"];
+                            //listItem.appendChild(thisCourse);
+                            tooltip.appendChild(thisCourse);
+                        }
                     }
+
+                    const courseCount = Object.keys(users[thisID]).length - 1;
+                    const text = document.createElement("sup");
+                    text.textContent = courseCount.toString();
+                    text.classList.add("tutooltip");
+                    text.appendChild(tooltip);
+
+                    //text.appendChild(div);
+                    user.parentElement.appendChild(text);
+
                 }
-
-                const courseCount = Object.keys(users[thisID]).length - 1;
-                const text = document.createElement("sup");
-                text.textContent = courseCount.toString();
-                text.classList.add("tutooltip");
-                text.appendChild(tooltip);
-
-                //text.appendChild(div);
-                user.parentElement.appendChild(text);
-
             }
         }
-    }
+    });
 });
 
--- js/menu.js
+++ js/menu.js
@@ -1,36 +1,51 @@
-document.addEventListener('DOMContentLoaded', function () {
-
-    // Initializing tabs
-    const settingsTabs = M.Tabs.init(document.getElementById("tucursos-tabs"));
-
-    const switchCC1 = document.getElementById("tucursos-cc-1");
-    switchCC1.addEventListener('change', function() {
-        alert(switchCC1.checked);
-    }, false);
-
-    const switchCC2 = document.getElementById("tucursos-cc-2");
-    switchCC2.addEventListener('change', function() {
-        alert(switchCC2.checked);
-    }, false);
-
-    const switchCC3 = document.getElementById("tucursos-cc-3");
-    switchCC3.addEventListener('change', function() {
-        alert(switchCC3.checked);
-    }, false);
-
-
-    const switchCCR = document.getElementById("tucursos-cc-r");
-    // Getting storage used and adding it above the delete data button
-    chrome.storage.local.getBytesInUse(null, function(data) {
-        const dataStored = document.createElement("span");
-        dataStored.textContent = (Math.round(100 * (data/1024)/1024)/100).toString() + "MB usados.";
-        dataStored.classList.add("right-align");
-        switchCCR.parentElement.appendChild(document.createElement("br"));
-        switchCCR.parentElement.appendChild(dataStored);
+/**
+ * Updates a setting to a new value
+ */
+function change_setting(key, value) {
+    chrome.storage.local.get("settings", function (data) {
+        const settings = data["settings"] ?? {};
+        settings[key] = value
+        chrome.storage.local.set({"settings": settings}, function () {
+            console.log("Setting updated.")
+        });
     });
+}
 
-    switchCCR.addEventListener('click', function() {
-        alert("data reset");
-        //chrome.storage.local.clear();
+
+/**
+ * Creates a switch and sets its value to the current setting
+ */
+function initialize_switch(settings, key) {
+    const this_switch = document.getElementById(key);
+    this_switch.checked = settings[key];
+    this_switch.addEventListener('change', function() {
+        change_setting(key, this_switch.checked)
     }, false);
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    chrome.storage.local.get("settings", function (data) {
+        const settings = data["settings"] ?? {};
+
+        // Page 1 Switches
+        initialize_switch(settings, "cc-show-counter");
+        initialize_switch(settings, "cc-save-participants");
+        initialize_switch(settings, "cc-show-saved-icon");
+
+        const switchCCR = document.getElementById("tucursos-cc-r");
+        // Getting storage used and adding it above the delete data button
+        chrome.storage.local.getBytesInUse(null, function(data) {
+            const dataStored = document.createElement("span");
+            dataStored.textContent = (Math.round(100 * (data/1024)/1024)/100).toString() + "MB usados.";
+            dataStored.classList.add("right-align");
+            switchCCR.parentElement.appendChild(document.createElement("br"));
+            switchCCR.parentElement.appendChild(dataStored);
+        });
+
+        switchCCR.addEventListener('click', function() {
+            alert("data reset");
+            //chrome.storage.local.clear();
+        }, false);
+    });
 }, false)
