644bc13cf50dc3c2a77c966d84033337c8a2cd64
Eric K <54124297+Nyveon@users.noreply.github.com>
2021-11-14 21:08:23 -0300

    Adds: Hide piechart and hide blog preview

--- js/background-settings.js
+++ js/background-settings.js
@@ -14,14 +14,14 @@ chrome.runtime.onInstalled.addListener(function() {
         "cc-show-counter": true,            // Toggle for showing shared course counter
         "cc-save-participants": true,       // Toggle for saving course participants
         "cc-show-saved-icon": false,        //todo Shows which courses have been scraped
-        "eh-hide-preview": false,           //todo Historial blog preview
+        "eh-hide-preview": false,           // Toggles hiding the Historial blog preview
         "eh-hide-sidebar": false,           //todo Hides the left-hand sidebar
-        "eh-hide-piechart": false,          //todo Hides the homework piechart block
+        "eh-hide-piechart": false,          // Toggles hiding the homework piechart block
         "eh-notification-bubble": false,    //todo Hides Notification bubbles
         "eh-anon-course": false,            //todo Censors course names
         "eh-anon-people": false,            //todo Censors people's names
     }
-    
+
     chrome.storage.local.set({"settings": settings}, function() {
         console.log("Default settings set successfully.");
     });
--- js/element-hider.js
+++ js/element-hider.js
@@ -1,4 +1,22 @@
-if(window.location.toString().match(".*:\/\/.*u-cursos.cl\/.*\/historial\/.*")){
-	document.querySelector(".sidebar").remove()
-	alert(test)
-}
+/**
+ * This script remove various elements from the page when the right page is matched.
+ */
+document.addEventListener("DOMContentLoaded", function(event) {
+	chrome.storage.local.get("settings", function (data) {
+		const settings = data["settings"] ?? {};
+
+		// Remove sidebar
+		if (settings["eh-hide-preview"]) {
+			if (window.location.toString().match(".*:\/\/.*u-cursos.cl\/.*\/historial\/.*")) {
+				document.querySelector(".sidebar").remove();
+			}
+		}
+
+		// Remove homework pie chart
+		if (settings["eh-hide-piechart"]) {
+			if (window.location.toString().match(".*:\/\/.*u-cursos.cl\/.*\/tareas_usuario\/.*")) {
+				document.querySelector('#body').children[1].remove();
+			}
+		}
+	});
+});
