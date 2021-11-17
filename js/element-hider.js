bc420c4f6d518602cfe1894e53147fb13edf9682
Eric K <54124297+Nyveon@users.noreply.github.com>
2021-11-17 19:27:48 -0300

    Changes all element hidings to MutationObserver

--- js/element-hider.js
+++ js/element-hider.js
@@ -1,22 +1,60 @@
 /**
  * This script remove various elements from the page when the right page is matched.
  */
-document.addEventListener("DOMContentLoaded", function(event) {
-	chrome.storage.local.get("settings", function (data) {
-		const settings = data["settings"] ?? {};
 
-		// Remove sidebar
-		if (settings["eh-hide-preview"]) {
-			if (window.location.toString().match(".*:\/\/.*u-cursos.cl\/.*\/historial\/.*")) {
-				document.querySelector(".sidebar").remove();
+
+/**
+ * Hides specific element given a selector
+ * @param n Element being searched
+ * @param selector Element target
+ */
+function hideElement(n, selector) {
+	if (n.firstElementChild && n.querySelector(selector)) {
+		for (const el of n.querySelectorAll(selector)) {
+			el.style.cssText += "display:none;";
+		}
+	}
+}
+
+
+chrome.storage.local.get("settings", function (data) {
+	const settings = data["settings"] ?? {};
+
+	const ehHidePiechart = window.location.toString().match(".*:\\/\\/.*u-cursos.cl\\/.*\\/tareas_usuario\\/.*") && settings["eh-hide-piechart"];
+	const ehHidePiechartSelector = '.detalle';
+
+	const ehHidePreview = window.location.toString().match(".*:\/\/.*u-cursos.cl\/.*\/historial\/.*") && settings["eh-hide-preview"];
+	const ehHidePreviewSelector = '.sidebar';
+
+	const mo = new MutationObserver(onMutation);
+	onMutation([{addedNodes: [document.documentElement]}]);
+	observe();
+
+	function onMutation(mutations) {
+		for (const {addedNodes} of mutations) {
+			for (const n of addedNodes) {
+				if (n.tagName) {
+
+					// Hide homework pie-chart
+					if (ehHidePiechart) {
+						hideElement(n, ehHidePiechartSelector);
+					}
+
+					// Hide blog preview sidebar
+					if (ehHidePreview) {
+						hideElement(n, ehHidePreviewSelector);
+					}
+				}
 			}
 		}
+	}
 
-		// Remove homework pie chart
-		if (settings["eh-hide-piechart"]) {
-			if (window.location.toString().match(".*:\/\/.*u-cursos.cl\/.*\/tareas_usuario\/.*")) {
-				document.querySelector('#body').children[1].remove();
+	function observe() {
+		mo.observe(document, {
+				subtree: true,
+				childList: true
 			}
-		}
-	});
+		);
+	}
 });
+
