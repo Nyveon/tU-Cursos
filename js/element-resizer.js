f201548f053eddca964c8e75db84c804f897319d
PuntitOwO <christopher.marin@ug.uchile.cl>
2021-11-17 18:57:41 -0300

    Bottom border added

--- js/element-resizer.js
+++ js/element-resizer.js
@@ -2,16 +2,11 @@
  * This script makes an element resizable from the page when the right page is matched.
  */
 document.addEventListener("DOMContentLoaded", function(event) {
-	chrome.storage.local.get("settings", function (data) {
-		const settings = data["settings"] ?? {};
 
-		// Resize pdf viewer
-		if (true) {
-			if (window.location.toString().match(".*:\/\/.*u-cursos.cl\/.*\/material_docente\/.+")) {
-                let obj = document.querySelector("object");
-                obj.style="width:100%;height:100%;";
-                obj.parentElement.style="resize:vertical;overflow:hidden;"
-			}
-		}
-	});
+    // Resize pdf viewer
+    if (window.location.toString().match(".*:\/\/.*u-cursos.cl\/.*\/material_docente\/.+")) {
+        let obj = document.querySelector("object");
+        obj.style="width:100%;height:100%;border-bottom: 0.8em solid black";
+        obj.parentElement.style="resize:vertical;overflow:hidden;"
+    }
 });
