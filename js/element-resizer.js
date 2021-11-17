f7f42bf7b695fea813feaf6f29d330122f681e6c
PuntitOwO <christopher.marin@ug.uchile.cl>
2021-11-17 19:41:55 -0300

    Bottom border color changed

--- js/element-resizer.js
+++ js/element-resizer.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // Resize pdf viewer
     if (window.location.toString().match(".*:\/\/.*u-cursos.cl\/.*\/material_docente\/.+")) {
         let obj = document.querySelector("object");
-        obj.style="width:100%;height:100%;border-bottom: 0.8em solid black";
+        obj.style="width:100%;height:100%;border-bottom: 0.8em solid #323639";
         obj.parentElement.style="resize:vertical;overflow:hidden;"
     }
 });
