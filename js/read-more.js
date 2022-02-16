11b603b698b3840bf9cba069f2d3a7381f2e98cf
sebcp <scaphismm@gmail.com>
2022-02-16 11:31:34 -0300

    Changed functionality, instead of using number of characters as threshold it uses number of paragraphs. TODO: Fix problem that erases double <br>'s

--- js/read-more.js
+++ js/read-more.js
@@ -1,25 +1,39 @@
 
-var char_limit = 400;
+var paragraph_limit = 5;
 var text = document.getElementsByClassName('texto');
 
-for(i=0; i<text.length; i++){
+function getCharLen(str){
+    str = str.replace(/<[^>]*>/g, "");
+    return str.length;
+}
+
+function countLines(elem){
+    var paragraphs = elem.innerHTML.split("<br>").filter(String);
+    var len = paragraphs.length;
+    if(len == 0){
+        return 1;
+    }
+    else{
+        return len;
+    }
+}
+
+for(var i = 0; i < text.length; i++){
     var opciones = text[i].lastElementChild;
     var removed = text[i].removeChild(opciones);
-
-    if(text[i].innerHTML.length >= char_limit){
-       text[i].innerHTML = '<div><span class="short-text">' + text[i].innerHTML.substring(0, char_limit) + '</span><span class="long-text" style="display: none">' + text[i].innerHTML.substring(char_limit) + '</span><br><button class="show-more-button" data-more="0">Ver más</span></div>';
+    var text_length = countLines(text[i])
+    if(text_length >= paragraph_limit){
+        var paragraphs = text[i].innerHTML.split("<br>").filter(String);
+        short_text = paragraphs[0];
+        for(var j = 1; j <= paragraph_limit; j++){
+            short_text = short_text + "<br>" + paragraphs[j];
+        }
+        long_text = text[i].innerHTML
+        text[i].innerHTML = '<div><span class="short-text">' + short_text + '</span><span class="long-text" style="display: none">' + long_text + '</span><br><button class="show-more-button" data-more="0">Ver más</span></div>';
     }
+
     var added = text[i].append(opciones)
 };
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, "");
-    createRegion();
-    createComuna("default");
-    setDate(1);
-});
 
 var buttons = document.querySelectorAll('.show-more-button')
 
