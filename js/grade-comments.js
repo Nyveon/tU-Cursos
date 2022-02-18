80fd503097c101ca7c74aa2be0ddc17b1dae18be
sebcp <scaphismm@gmail.com>
2022-02-18 19:28:52 -0300

    Added grade commentaries popup.

---
+++ js/grade-comments.js
@@ -0,0 +1,13 @@
+/*
+* Query all a hrefs that belong to students grades and then add
+* attributes target and onclick so it opens a popup instead of
+* changing the page when clicking on the link.
+*/
+
+var elem = document.querySelectorAll('a[href*="historial?id_evaluacion="]');
+
+for (i = 0; i < elem.length; i++){
+    var link = elem[i].attributes[i].nodeValue;
+    elem[i].setAttribute("target", "popup")
+    elem[i].setAttribute("onclick", "window.open('" + link + "','popup','width=600,height=600'); return false;")
+}
