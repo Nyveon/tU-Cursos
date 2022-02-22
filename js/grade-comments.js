66d81570b1af1161d8349af909d698a2e6b3a211
sebcp <scaphismm@gmail.com>
2022-02-22 14:00:41 -0300

    Fixed little mistake.

--- js/grade-comments.js
+++ js/grade-comments.js
@@ -7,7 +7,7 @@
 var elem = document.querySelectorAll('a[href*="historial?id_evaluacion="]');
 
 for (i = 0; i < elem.length; i++){
-    var link = elem[i].attributes[i].nodeValue;
+    var link = elem[i].attributes[0].nodeValue;
     elem[i].setAttribute("target", "popup")
     elem[i].setAttribute("onclick", "window.open('" + link + "','popup','width=600,height=600'); return false;")
 }
