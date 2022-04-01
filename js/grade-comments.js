40b62655bd3a16bd16513203084fd4acc193960a
Eric K <54124297+Nyveon@users.noreply.github.com>
2022-04-01 02:21:53 -0300

    feat(grade-comments): make toggleable

--- js/grade-comments.js
+++ js/grade-comments.js
@@ -4,10 +4,19 @@
 * changing the page when clicking on the link.
 */
 
-var elem = document.querySelectorAll('a[href*="historial?id_evaluacion="]');
 
-for (i = 0; i < elem.length; i++){
-    var link = elem[i].attributes[0].nodeValue;
-    elem[i].setAttribute("target", "popup")
-    elem[i].setAttribute("onclick", "window.open('" + link + "','popup','width=600,height=600'); return false;")
-}
+chrome.storage.local.get("settings", function (data) {
+    const settings = data["settings"] ?? {};
+
+    if (!settings["qol-grade-comments"]) {
+        return
+    }
+
+    var elem = document.querySelectorAll('a[href*="historial?id_evaluacion="]');
+
+    for (i = 0; i < elem.length; i++){
+        var link = elem[i].attributes[0].nodeValue;
+        elem[i].setAttribute("target", "popup")
+        elem[i].setAttribute("onclick", "window.open('" + link + "','popup','width=600,height=600'); return false;")
+    }
+});
