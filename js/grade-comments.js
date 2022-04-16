5b6a6e44ce4a13ce84e19c2e7edba71ac06c150f
Eric K <54124297+Nyveon@users.noreply.github.com>
2022-04-16 17:50:51 -0400

    refactor: grade-comments. linting.

--- js/grade-comments.js
+++ js/grade-comments.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /*
 * Query all a hrefs that belong to students grades and then add
 * attributes target and onclick so it opens a popup instead of
@@ -6,17 +8,17 @@
 
 
 chrome.storage.local.get("settings", function (data) {
-    const settings = data["settings"] ?? {};
+    const settings = data.settings ?? {};
 
     if (!settings["qol-grade-comments"]) {
-        return
+        return;
     }
 
     var elem = document.querySelectorAll('a[href*="historial?id_evaluacion="]');
 
-    for (i = 0; i < elem.length; i++){
+    for (let i = 0; i < elem.length; i++){
         var link = elem[i].attributes[0].nodeValue;
-        elem[i].setAttribute("target", "popup")
-        elem[i].setAttribute("onclick", "window.open('" + link + "','popup','width=600,height=600'); return false;")
+        elem[i].setAttribute("target", "popup");
+        elem[i].setAttribute("onclick", "window.open('" + link + "','popup','width=600,height=600'); return false;");
     }
 });
